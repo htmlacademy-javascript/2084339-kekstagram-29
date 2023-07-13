2874620355f7f926df1a26dc1f12b24016865bbf
OlgaFeish <108367203+OlgaFeish@users.noreply.github.com>
2023-07-13 17:13:26 +0300

    fix names

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -4,8 +4,8 @@ const containerElement = document.querySelector('.pictures');
 const createThumbnail = ({ url, description, likes, comments }) => {
   const thumbnail = thumbnailTemplate.cloneNode(true);
 
-  thumbnail.querySelector('.picture__url').src = url;
-  thumbnail.querySelector('.picture__description').alt = description;
+  thumbnail.querySelector('.picture__img').src = url;
+  thumbnail.querySelector('.picture__img').alt = description;
   thumbnail.querySelector('.picture__comments').textContent = comments.length ;
   thumbnail.querySelector('.picture__likes').textContent = likes;
 
